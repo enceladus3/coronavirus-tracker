ae9bc2f2588a50e4d674a4c2be05471aeb951fcd
enceladus3 <6pcw-13j!hgXg>
2020-03-28 21:39:25 +0200

    Initial commit

---
+++ src/test/java/group/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package group.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
